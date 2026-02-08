3c249f9a40600cd0b3ca3b9d7e6dd250f08b1271
Armaan-khan786 <a75777129@gmail.com>
2026-02-08 15:21:32 +0530

    Update sender.ino

--- sender/sender.ino
+++ sender/sender.ino
@@ -3,22 +3,23 @@
 
 void setup() {
   Serial.begin(115200);
-  delay(2000);
+  delay(3000);   // allow full boot after flashing
 
   pinMode(SIGNAL_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
+
+  digitalWrite(SIGNAL_PIN, LOW);   // force known start state
+  digitalWrite(LED_PIN, LOW);
 
   Serial.println("Sender Ready");
 }
 
 void loop() {
-  // HIGH
   digitalWrite(SIGNAL_PIN, HIGH);
   digitalWrite(LED_PIN, HIGH);
   Serial.println("[SENDER] 1");
   delay(500);
 
-  // LOW
   digitalWrite(SIGNAL_PIN, LOW);
   digitalWrite(LED_PIN, LOW);
   Serial.println("[SENDER] 0");
